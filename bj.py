32e2c4888cae661737ca646b592eb76f7341fb84
Caden Claussen <caden.claussen@gmail.com>
2020-10-17 11:55:43 -0700

    Fixed for loop in deck

--- bj.py
+++ bj.py
@@ -14,7 +14,8 @@ class Card:
 class Deck:
 
     def __init__(self):
-        self.cards = [ "H", "D", "S", "C" ]
+        self.cards = []
+        for suit in [ "H", "D", "S", "C" ]:
             for value in [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" ]:
                 card = Card(value, suit)
                 self.cards.append(card)
